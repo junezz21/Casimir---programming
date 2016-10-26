ed802969bf561a65c528868590598ef355670d2b
junezz21 <z.zhu-1@tudelft.nl>
2016-10-26 11:42:47 +0000

    commit message

--- test.py
+++ test.py
@@ -10,11 +10,13 @@ def surface(radius)
 radius = 2
 print("The surface is", b)
 print("The circumference is",a)
+#version 3
 =======
     a = 2*pi*radius
     print("The circumference is",a)
+    
 =======
 hello world #comment
-#comment1
+
 >>>>>>> 7dc25b1f688cafc0e0a1b03f895efa2d9a94ef80
 >>>>>>> 1bc1a7c786920a435c935b8e2887f057d744fc15
